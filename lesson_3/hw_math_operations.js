214627284af848d20ef5d673aed0bea5112602c4
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2022-10-31 20:41:21 +0200

    fix remarks

--- lesson_3/hw_math_operations.js
+++ lesson_3/hw_math_operations.js
@@ -2,10 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
   let firstNumber = Number(prompt('Type first number: '));
   let secondNumber = Number(prompt('Type second number: '));
 
-  resultSummary = firstNumber + secondNumber;
-  resultSubstraction = firstNumber - secondNumber;
-  resultMultiply = firstNumber * secondNumber;
-  resultDivide = firstNumber / secondNumber;
+  let resultSummary = firstNumber + secondNumber;
+  let resultSubstraction = firstNumber - secondNumber;
+  let resultMultiply = firstNumber * secondNumber;
+  let resultDivide = firstNumber / secondNumber;
 
-  alert(`${firstNumber} + ${secondNumber} = ${resultSummary}\n${firstNumber} - ${secondNumber} = ${resultSubstraction}\n${firstNumber} * ${secondNumber} = ${resultMultiply}\n${firstNumber} / ${secondNumber} = ${resultDivide}\n`)
+  alert(`${firstNumber} + ${secondNumber} = ${resultSummary}\n
+  ${firstNumber} - ${secondNumber} = ${resultSubstraction}\n
+  ${firstNumber} * ${secondNumber} = ${resultMultiply}\n
+  ${firstNumber} / ${secondNumber} = ${resultDivide}\n`)
 })
