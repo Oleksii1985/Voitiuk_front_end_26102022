dfbbb48d71f2a5144eafd63d434ebcf4a6d2e7a9
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2022-11-02 14:30:41 +0200

    Fix remarks

--- lesson_4/customerForm.js
+++ lesson_4/customerForm.js
@@ -1,53 +1,59 @@
-document.addEventListener('DOMContentLoaded', () => {
+const userBirthDate = prompt('Якого Ви року народження?: ');
+const userHometown = prompt('В якому місті Ви живете?: ');
+const userFavoriteSport = prompt('Який Ваш улюблений вид спорту?: ');
 
-  alert('!!!Увага!!! Усі дані треба вводити з заглавної літери!!!')
+const kyiv = 'Київ';
+const washington = 'Вашингтон';
+const london = 'Лондон';
 
-  const userBirthDate = prompt('Якого Ви року народження?: ');
-  const userHometown = prompt('В якому місті Ви живете?: ');
-  const userFavoriteSport = prompt('Який Ваш улюблений вид спорту?: ');
+const ukraine = 'України';
+const usa = 'Сполучених штатів';
+const england = 'Англії';
 
-  const kyiv = 'Київ';
-  const washington = 'Вашингтон';
-  const london = 'Лондон';
+const football = 'Футбол';
+const box = 'Бокс';
+const formula = 'Формула 1';
 
-  const football = 'Футбол';
-  const box = 'Бокс';
-  const formula = 'Формула 1';
-
-  const pele = 'Пеле';
-  const tyson = 'Тайсоном';
-  const shumacher = 'Шумахером';
-
-  function sportMessage(userFavoriteSport) {
-    if (userFavoriteSport === football) {
-      return `Круто! Хочеш стати ${pele}?`;
-    } else if (userFavoriteSport === box) {
-      return `Круто! Хочеш стати ${tyson}?`;
-    } else if (userFavoriteSport === formula) {
-      return `Круто! Хочеш стати ${shumacher}?`;
-    } else {
-      return 'Щодо цього виду спорту немає інформації'
-    }
-  }
-
-  if (!userBirthDate) {
-    alert(`Шкода, що Ви не захотіли ввести свій рік народження`);
-  }
-  if (userHometown === null) {
-    alert(`Шкода, що Ви не захотіли ввести назву свого міста`);
-  }
-  if (userFavoriteSport === null) {
-    alert(`Шкода, що Ви не захотіли ввести свій улюблений вид спорту`);
-  }
+const pele = 'Пеле';
+const tyson = 'Тайсоном';
+const shumacher = 'Шумахером';
 
 const userHometownLowerRegister = userHometown.toLowerCase()
-  if (userHometownLowerRegister === kyiv.toLowerCase() || ...) {
-    alert(`Твій рік народження: ${userBirthDate}\n
-    Ти живеш у столиці ${userHometown}\n
-    ${sportMessage(userFavoriteSport)}`);
+const userFavoriteSportLowerRegister = userFavoriteSport.toLowerCase()
+
+function countryMessage(userHometownLowerRegister) {
+  if (userHometownLowerRegister === kyiv.toLowerCase()) {
+    return result = `Ти живеш у столиці ${ukraine}`;
+  } else if (userHometownLowerRegister === washington.toLowerCase()) {
+    return result = `Ти живеш у столиці ${usa}`;
+  } else if (userHometownLowerRegister === london.toLowerCase()) {
+    return result = `Ти живеш у столиці ${england}`;
   } else {
-    alert(`Твій рік народження: ${userBirthDate}\n
-    Ти живеш у місті ${userHometown}\n
-    ${sportMessage(userFavoriteSport)}`);
+    return result = `Ти живеш у місті ${userHometown}`
+  };
+}
+function sportMessage(userFavoriteSportLowerRegister) {
+  if (userFavoriteSportLowerRegister === football.toLowerCase()) {
+    return result = `Круто! Хочеш стати ${pele}?`;
+  } else if (userFavoriteSportLowerRegister === box.toLowerCase()) {
+    return result = `Круто! Хочеш стати ${tyson}?`;
+  } else if (userFavoriteSportLowerRegister === formula.toLowerCase()) {
+    return result = `Круто! Хочеш стати ${shumacher}?`;
+  } else {
+    return result = 'Щодо цього виду спорту немає інформації'
   }
-})
+}
+
+if (!userBirthDate) {
+  alert(`Шкода, що Ви не захотіли ввести свій рік народження`);
+}
+if (!userHometown) {
+  alert(`Шкода, що Ви не захотіли ввести назву свого міста`);
+}
+if (!userFavoriteSport) {
+  alert(`Шкода, що Ви не захотіли ввести свій улюблений вид спорту`);
+}
+
+alert(`Твій рік народження: ${userBirthDate}\n
+${countryMessage(userHometownLowerRegister)}\n
+${sportMessage(userFavoriteSport)}`);
