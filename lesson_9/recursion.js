a63fac07bed7901cfa5a292c9265da0f4df4f959
Oleksii Voitiuk <dolphin.avoytyuk@gmail.com>
2022-11-28 17:43:06 +0200

    fix issue with negative pow

--- lesson_9/recursion.js
+++ lesson_9/recursion.js
@@ -5,8 +5,18 @@
  */
 
 function numberToPow(number, pow) {
-  const result = pow === 0 ? 1 : number * numberToPow(number, --pow);
-  return result
+  if (pow >= 0) {
+    const result = pow === 0 ? 1 : number * numberToPow(number, --pow);
+    return result;
+  } else {
+    if (pow === -1) {
+      const result = 1.0 / number;
+      return result;
+    } else {
+      const result = 1.0 / (number * numberToPow(number, (-1) * (pow + 1)));
+      return result;
+    }
+  }
 }
 
-console.log(numberToPow(2, 2));
+console.log(numberToPow(2, -2));
